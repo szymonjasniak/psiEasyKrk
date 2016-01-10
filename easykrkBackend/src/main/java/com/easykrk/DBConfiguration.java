58fa3dc6338c32e92b226788ebf49eeb7df0e3e7
ABAJDIUK <adrian.bajdiuk@gmail.com>
2016-01-10 17:08:08 +0100

    Database configuration added

---
+++ easykrkBackend/src/main/java/com/easykrk/DBConfiguration.java
@@ -0,0 +1,37 @@
+package com.easykrk;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfiguration {
+
+	@Value("${db.host}")
+	String host;
+	
+	@Value("${db.port}")
+	String port;
+	
+	@Value("${db.database.name}")
+	String dbName;
+	
+	@Value("${db.username}")
+	String user;
+	
+	@Value("${db.password}")
+	String password;
+	
+	@Bean
+	public DataSource dataSource(){
+		
+		DataSourceBuilder dbSourceBuilder=DataSourceBuilder.create();
+		
+		String url="jdbc:postgresql://" + host + ':' + port + '/' + dbName+"?sslmode=require";
+		return dbSourceBuilder.url(url).username(user).password(password).build();
+	}
+	
+}
