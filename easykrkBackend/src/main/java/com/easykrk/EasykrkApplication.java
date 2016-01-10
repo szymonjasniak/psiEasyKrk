d94258ac2c661f17e0ea676c4020b599e1911f13
Szymon Jaśniak <szymon.jasniak@capgemini.com>
2016-01-10 14:39:09 +0100

    Initial commit

---
+++ easykrkBackend/src/main/java/com/easykrk/EasykrkApplication.java
@@ -0,0 +1,12 @@
+package com.easykrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasykrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasykrkApplication.class, args);
+	}
+}
