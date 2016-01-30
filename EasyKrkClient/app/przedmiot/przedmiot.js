b9bae8cc44703471137176e944aeae80fd753117
Szymon Jaśniak <szymonjasniak@gmail.com>
2016-01-30 21:46:44 +0100

    making table editable

--- EasyKrkClient/app/przedmiot/przedmiot.js
+++ EasyKrkClient/app/przedmiot/przedmiot.js
@@ -5,7 +5,7 @@ angular.module('application.przedmiot', [])
 	
 	$scope.zajecia = [
 	{
-        "forma": 'Wwewe',        
+        "forma": 'W',        
         "liczba": '2',
         "ects": '3',
         "cnps": '2',
@@ -28,8 +28,43 @@ angular.module('application.przedmiot', [])
     }
     ];
 
+	$scope.gridOptions = {}
+	$scope.gridOptions.enableCellEditOnFocus = true;
 	$scope.gridOptions = { 
-	        data: 'zajecia'
+	        data: 'zajecia',
+	        columnDefs: [{field:'forma', displayName:'Forma', width:70},
+	                     {field:'liczba', displayName:'Liczba'},
+	                     {field:'ects', displayName:'ECTS', width:70},
+	                     {field:'cnps', displayName:'CNPS', width:70},
+	                     {field:'zaliczenie', displayName:'Zaliczenie', width:100},
+	                     {field:'semestr', displayName:'Semestr', width:100}]
 	    };
+	
+	
+	$scope.addData = function() {
+	    var n = $scope.gridOptions.data.length + 1;
+	    $scope.gridOptions.data.push({
+	                "forma": "New " + n,
+	                "liczba": "Person " + n,
+	                "ects": "abc",
+	                "cnps": "abc",
+	                "zaliczenie": 'Z',
+	                "semestr": '1'
+	              });
+	  };
+	 
+	  $scope.removeFirstRow = function() {
+	    //if($scope.gridOpts.data.length > 0){
+	       $scope.gridOptions.data.splice(0,1);
+	    //}
+	  };
+	 
+	  $scope.reset = function () {
+	    data1 = angular.copy(origdata1);
+	    data2 = angular.copy(origdata2);
+	 
+	    $scope.gridOptions.data = data1;
+	    $scope.gridOptions.columnDefs = columnDefs1;
+	  }
 	
 });
