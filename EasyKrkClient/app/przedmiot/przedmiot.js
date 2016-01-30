12b0e7d482f3fa91af3454a83bb082215da78719
Szymon Jaśniak <szymonjasniak@gmail.com>
2016-01-31 00:48:27 +0100

    Not finished przedmiot

--- EasyKrkClient/app/przedmiot/przedmiot.js
+++ EasyKrkClient/app/przedmiot/przedmiot.js
@@ -3,23 +3,25 @@ angular.module('application.przedmiot', [])
 	
 	$scope.przedmiot = {program: 'guest', cykl: '2015/2016', nazwaPolska:"Nazwa"};
 	
+	$scope.possibleForms = [{id: 1, 'forma':"W"},{id: 2 ,'forma':"C"},{id: 3, 'forma':"L"},{id: 4, 'forma':"S"},{id: 5, 'forma':"P"}];
+	
 	$scope.zajecia = [
 	{
-        "forma": 'W',        
+        "forma": '1',        
         "liczba": '2',
         "ects": '3',
         "cnps": '2',
         "zaliczenie": 'E',
         "semestr": '1'
     }, {
-        "forma": 'L',
+        "forma": '3',
         "liczba": '2',
         "ects": '4',
         "cnps": '3',
         "zaliczenie": 'Z',
         "semestr": '1'
     }, {
-        "forma": 'C',
+        "forma": '2',
         "liczba": '2',
         "ects": '3',
         "cnps": '2',
@@ -27,44 +29,67 @@ angular.module('application.przedmiot', [])
         "semestr": '1'
     }
     ];
-
-	$scope.gridOptions = {}
+	
+	$scope.ek = [];
+  
+	$scope.gridOptions = {};
 	$scope.gridOptions.enableCellEditOnFocus = true;
 	$scope.gridOptions = { 
-	        data: 'zajecia',
-	        columnDefs: [{field:'forma', displayName:'Forma', width:70},
-	                     {field:'liczba', displayName:'Liczba'},
-	                     {field:'ects', displayName:'ECTS', width:70},
-	                     {field:'cnps', displayName:'CNPS', width:70},
-	                     {field:'zaliczenie', displayName:'Zaliczenie', width:100},
-	                     {field:'semestr', displayName:'Semestr', width:100}]
+	        data: $scope.zajecia,
+	        enableHorizontalScrollbar : 0,
+	        enableVerticalScrollbar   : 0,
+	        columnDefs: [{field:'forma', displayName:i18n.t("przedmiot.forma"), width:70, cellFilter: 'mapFormy',editableCellTemplate: 'ui-grid/dropdownEditor', editDropdownValueLabel: 'forma', editDropdownOptionsArray: $scope.possibleForms},
+	                     {field:'liczba', displayName:i18n.t("przedmiot.liczba")},
+	                     {field:'ects', displayName:i18n.t("przedmiot.ects"), width:70},
+	                     {field:'cnps', displayName:i18n.t("przedmiot.cnps"), width:70},
+	                     {field:'zaliczenie', displayName:i18n.t("przedmiot.zaliczenie"), width:100},
+	                     {field:'semestr', displayName:i18n.t("przedmiot.semestr"), width:100}]
+	
 	    };
 	
+	$scope.gridOptionsEK = {};
+	$scope.gridOptionsEK.enableCellEditOnFocus = true;
+	$scope.gridOptionsEK = { 
+	        data: $scope.ek,
+	        enableHorizontalScrollbar : 0,
+	        enableVerticalScrollbar   : 0,
+	        columnDefs: [{field:'id', displayName:i18n.t("efekt.id")},
+	                     {field:'opis', displayName:i18n.t("efekt.opis")},
+	                     {field:'kategoria', displayName:i18n.t("efekt.kategoria")}]
+	
+	    };
 	
 	$scope.addData = function() {
-	    var n = $scope.gridOptions.data.length + 1;
-	    $scope.gridOptions.data.push({
-	                "forma": "New " + n,
-	                "liczba": "Person " + n,
-	                "ects": "abc",
-	                "cnps": "abc",
-	                "zaliczenie": 'Z',
-	                "semestr": '1'
-	              });
+	    var n = $scope.gridOptions.data.length;
+	    if(n >= $scope.possibleForms.length){
+	    	
+	    }
+	    else{
+	    	$scope.gridOptions.data.push({"forma": "",
+                "liczba": "",
+                "ects": "",
+                "cnps": "",
+                "zaliczenie": '',
+                "semestr": ''
+              });
+	    }	    
 	  };
-	 
-	  $scope.removeFirstRow = function() {
-	    //if($scope.gridOpts.data.length > 0){
-	       $scope.gridOptions.data.splice(0,1);
-	    //}
-	  };
-	 
-	  $scope.reset = function () {
-	    data1 = angular.copy(origdata1);
-	    data2 = angular.copy(origdata2);
-	 
-	    $scope.gridOptions.data = data1;
-	    $scope.gridOptions.columnDefs = columnDefs1;
-	  }
 	
+})
+.filter('mapFormy', function() {
+  var genderHash = {
+    1: 'W',
+    2: 'C',
+    3: 'L',
+    4: 'S',
+    5: 'P'
+  };
+ 
+  return function(input) {
+    if (!input){
+      return '';
+    } else {
+      return genderHash[input];
+    }
+  };
 });
