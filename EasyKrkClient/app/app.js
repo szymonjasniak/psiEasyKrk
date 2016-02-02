9295d806198dab772ef98c64d0240b06f4ed8247
Szymon Jaśniak <szymonjasniak@gmail.com>
2016-02-02 22:44:58 +0100

    Coś nagrzebałem

--- EasyKrkClient/app/app.js
+++ EasyKrkClient/app/app.js
@@ -35,34 +35,3 @@ var app = angular.module('application', ['ngRoute','ngTouch', 'ui.grid','applica
 	}
 })
 ;
-=======
-	})
-	.when('/listaPrzedmioty', {
-		templateUrl : 'app/przedmiot/listaPrzedmioty.html',
-		controller  : 'ListaPrzedmiotController'
-	})
-	.when('/kierunkowe',{
-		templateUrl: "app/KEK/template/KEK.html",
-		controller: "KEKController"
-	})
-	.otherwise({
-		redirectTo	: '/'
-	})
-})
-.service('translateService', function(){
-	language_complete = navigator.language.split("-");
-	language = (language_complete[0]);
-	console.log("Language (service): %s", language);
-	var i18 = i18n.init({ lng: language, debug: true, getAsync:false });
-})
-.directive("translate", function(translateService){
-	return function(scope, element, attrs){
-			  if(attrs.typeofelement){
-				  element.attr(attrs.typeofelement, i18n.t(attrs.text));
-			  }
-			  else{
-				  element.text(i18n.t(attrs.text));  
-			  }		
-	}
-})
->>>>>>> branch 'master' of https://github.com/szymonjasniak/psiEasyKrk.git
