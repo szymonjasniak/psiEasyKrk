416cba87c6f4b4c47566d509cfd4d5c92248b8bf
Szymon Jaśniak <szymonjasniak@gmail.com>
2016-02-03 19:23:07 +0100

    Init commit for backend

---
+++ easykrkBackend/src/main/java/com/easykrk/infrastructure/repository/PrzedmiotRepository.java
@@ -0,0 +1,9 @@
+package com.easykrk.infrastructure.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.easykrk.domain.model.Przedmiot;
+
+public interface PrzedmiotRepository extends CrudRepository<Przedmiot, Long> {
+
+}
